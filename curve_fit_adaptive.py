66ab9af8bbb44e65cca170d6799e54b303d2efd6
Daniel-VO <71396117+Daniel-VO@users.noreply.github.com>
2023-10-27 09:53:47 +0200

    Add files via upload

--- curve_fit_adaptive.py
+++ curve_fit_adaptive.py
@@ -5,65 +5,73 @@ This program is free software: you can redistribute it and/or modify it under th
 """
 
 import glob
-import lmfit
-import numpy
+import lmfit as lm
+import numpy as np
 import os
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import scipy
 from scipy import stats
 
-# ~ x=numpy.linspace(1,100,num=1000)
-# ~ y=(numpy.sin(x/5/numpy.pi))**2+1*(5**2/((x-70)**2+5**2))
+os.system('mv results.log results.alt')
+log=open('results.log','a')
 
-x,y=numpy.genfromtxt('PTMOS_FTIR.txt',unpack=True)
-args=numpy.where((x>600)&(x<900))
-x=x[args]
-y=y[args]
+for f in glob.glob('*.csv'):
+	filename=os.path.splitext(f)[0]
+	x,y=np.genfromtxt(f,delimiter=',',unpack=True,skip_header=1,usecols=(0,2),encoding='iso-8859-1')
+	args=np.where((x>100)&(x<800))
+	x=x[args]
+	y=y[args]
+	y-=y[0]
 
-def basis(baseline):
-	return baseline*numpy.ones(len(x))
-def fitfunc(x,height,center,width):
-	# ~ return height/(width*(2*numpy.pi)**0.5)*numpy.exp(-1./2*((x-center)/width)**2)					#Gauss Dichte
-	# ~ return -height/2*(1+scipy.special.erf((x-center)/(width*2**0.5)))								#Gauss Kumulativ
-	return height*(width**2/((x-center)**2+width**2))												#Lorentz Dichte
-	# ~ return height/(x*width*(2*numpy.pi)**0.5)*numpy.exp(-(numpy.log(x)-center)**2/(2*width**2))		#Log-normal
+	def basis(baseline):
+		return baseline*np.ones(len(x))
+	def fitfunc(x,height,center,width):
+		# ~ return height/(width*(2*np.pi)**0.5)*np.exp(-1./2*((x-center)/width)**2)					#Gauss Dichte
+		return -height/2*(1+scipy.special.erf((x-center)/(width*2**0.5)))								#Gauss Kumulativ
+		# ~ return height*(width**2/((x-center)**2+width**2))											#Lorentz Dichte
+		# ~ return height/(x*width*(2*np.pi)**0.5)*np.exp(-(np.log(x)-center)**2/(2*width**2))			#Log-normal
 
-Fehler=0.95	#Anpassen
-tau,i,params=0,0,lmfit.Parameters()
-params.add('baseline',0)
-while tau<Fehler or numpy.isnan(tau):
-	i+=1
-	for n in range(i):
-		params.add('height'+str(n),1,min=0)
-		params.add('center'+str(n),(max(x)+min(x))*(n+1)/(i+2),min=min(x),max=max(x))
-		params.add('width'+str(n),1,min=0)
-	def multi_fitfunc(params):
-		prm=params.valuesdict()
-		global func
-		func=basis(prm['baseline'])
+	Kriterium=1e-5	#Anpassen
+	Konvergenz=Kriterium+1
+	tau,i,params=0,0,lm.Parameters()
+	params.add('baseline',0,vary=False)
+	while Konvergenz>Kriterium:
+		i+=1
 		for n in range(i):
-			func+=fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)])
-		global res
-		res=func-y
-		return res
-	result=lmfit.minimize(multi_fitfunc,params,method='least_squares')
-	tau=stats.kendalltau(func,y)[0]
-	print('i:',i,'tau:',tau)
-	prm=result.params.valuesdict()
-	####
-	# ~ result.params.pretty_print()
-	# ~ plt.plot(x,y)
-	# ~ plt.plot(x,func)
-	# ~ for n in range(i):
-		# ~ plt.plot(x,fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)]))
-	# ~ plt.show()
-	####
+			params.add('height'+str(n),1,min=0,max=2*max(abs(y)))
+			params.add('center'+str(n),(max(x)+min(x))*(n+1)/(i+2))
+			params.add('width'+str(n),1,min=0,max=2*max(abs(x)))
+		def multi_fitfunc(params):
+			prm=params.valuesdict()
+			global func
+			func=basis(prm['baseline'])
+			for n in range(i):
+				func+=fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)])
+			global res
+			res=func-y
+			return res
+		result=lm.minimize(multi_fitfunc,params,method='least_squares')
+		Konvergenz=np.var(res)/np.var(y)
+		print(filename,'i:',i,'Konvergenz:',Konvergenz)
 
-result.params.pretty_print()
-prm=result.params.valuesdict()
-plt.plot(x,y)
-plt.plot(x,func)
-for n in range(i):
-	plt.plot(x,fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)]))
-plt.show()
+		####
+		# ~ prm=result.params.valuesdict()
+		# ~ result.params.pretty_print()
+		# ~ plt.close('all')
+		# ~ plt.plot(x,y)
+		# ~ plt.plot(x,func)
+		# ~ for n in range(i):
+			# ~ plt.plot(x,fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)]))
+		# ~ plt.show()
+		####
+
+	log.write(filename+': '+str(result.params.valuesdict())+'\n')
+
+	plt.close('all')
+	prm=result.params.valuesdict()
+	plt.plot(x,y)
+	plt.plot(x,func,linestyle='dashed')
+	for n in range(i):
+		plt.plot(x,fitfunc(x,prm['height'+str(n)],prm['center'+str(n)],prm['width'+str(n)]))
+	plt.savefig(filename+'.png',dpi=300)
